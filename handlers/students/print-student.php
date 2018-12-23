d3cb7d1afb236930648d4dc94e709f73ab539f03
Jp <johnpaulbalanon6@gmail.com>
2018-12-23 23:04:59 +0800

    pdf

---
+++ handlers/students/print-student.php
@@ -0,0 +1,22 @@
+<?php
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+require_once '../../db.php';
+
+$con = new pdo_db();
+
+$id = $_POST['id'];
+
+$enrollment = $con->getData("SELECT *, DATE_FORMAT(dob, '%M %d, %Y') dob FROM enrollment WHERE id = $_POST[id]");
+
+foreach ($enrollment[0] as $i => $p) {
+	
+	if ($p == null) $enrollment[0][$i] = ""; // pdf equals null
+	
+}
+
+header("Content-Type: application/json");
+echo json_encode($enrollment[0]);
+
+?>
