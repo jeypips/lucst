9408ae90bc9c442d0d6b5d0899670a411f4fb110
Jp <johnpaulbalanon6@gmail.com>
2018-12-27 15:45:42 +0800

    ..

--- api/suggestions/courses-check.php
+++ api/suggestions/courses-check.php
@@ -16,8 +16,8 @@ foreach ($courses as $key => $course) {
 
 	foreach ($curriculum as $i => $c) {
 		
-		$curriculum_data = $con->getData("SELECT id, curriculum_id, descriptive_title FROM curriculum_data WHERE curriculum_id = ".$c['id']);
-		$curriculum[$i]['curriculum_data'] = $curriculum_data;
+		$students_curriculum_datas = $con->getData("SELECT id, curriculum_id, descriptive_title FROM curriculum_data WHERE curriculum_id = ".$c['id']);
+		$curriculum[$i]['students_curriculum_datas'] = $students_curriculum_datas;
 
 	};
 
--- handlers/enrollment/view.php
+++ handlers/enrollment/view.php
@@ -11,7 +11,24 @@ $con = new pdo_db();
 $enrollment = $con->getData("SELECT *, DATE_FORMAT(date_of_enrollment, '%M %d, %Y') date_of_enrollment FROM enrollment WHERE id = $_POST[id]");
 
 $course = $con->getData("SELECT * FROM courses WHERE id = ".$enrollment[0]['course']);
-$enrollment[0]['course'] = $course[0];	
+$enrollment[0]['course'] = $course[0];
+
+$curriculum = $con->getData("SELECT id, semester FROM curriculum WHERE id = ".$enrollment[0]['semester']);
+$enrollment[0]['course']['curriculum'] = $curriculum[0];
+
+$students_curriculum_datas = $con->getData("SELECT * FROM students_curriculum_data WHERE enrollment_id = ".$enrollment[0]['id']);
+
+foreach($students_curriculum_datas as $key => $scd){
+	
+	$curriculum_data = $con->getData("SELECT id, descriptive_title FROM curriculum_data WHERE id  = ".$scd['curriculum_data_id']);
+	$students_curriculum_datas[$key]['curriculum_data_id'] = $curriculum_data[0];
+	
+};
+
+$enrollment[0]['students_curriculum_datas'] = $students_curriculum_datas;
+$enrollment[0]['students_curriculum_dels'] = [];	
+	
+
 
 header("Content-Type: application/json");
 echo json_encode($enrollment[0]);
