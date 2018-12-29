db17e85968ff6cb4a7d879882c609a6aae4f069c
Jp <johnpaulbalanon6@gmail.com>
2018-12-29 09:08:39 +0800

    ..

--- handlers/students/print-student.php
+++ handlers/students/print-student.php
@@ -10,11 +10,21 @@ $id = $_POST['id'];
 
 $enrollment = $con->getData("SELECT *, DATE_FORMAT(dob, '%M %d, %Y') dob, DATE_FORMAT(date_of_enrollment, '%Y') date_of_enrollment FROM enrollment WHERE id = $_POST[id]");
 
+$students_curriculum_datas = $con->getData("SELECT * FROM curriculum_data");
+$enrollment[0]['students_curriculum_datas'] = $students_curriculum_datas;
+
+/* foreach($students_curriculum_datas as $key => $students_curriculum_data){
+	
+	$curriculum_data = $con->getData("SELECT * FROM curriculum_data WHERE id = ".$students_curriculum_data['curriculum_data_id']);
+	$students_curriculum_datas[$key]['curriculum_data_id'] = $curriculum_data;
+	
+}; */
+
 foreach ($enrollment[0] as $i => $p) {
 	
 	if ($p == null) $enrollment[0][$i] = ""; // pdf equals null
 	
-}
+};
 
 header("Content-Type: application/json");
 echo json_encode($enrollment[0]);
